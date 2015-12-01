4e324482e8561465b54cab783168feae4def2c6b
Brendan Mahoney <BMahoney08@gmail.com>
2015-12-01 09:58:32 -0500

    edit view created

--- models/compliment.js
+++ models/compliment.js
@@ -9,14 +9,14 @@ function Compliment(){
 
 Compliment.all = function(){
   return compliments;
-}
+};
 
 Compliment.create = function(compliment){
   compliments.push(compliment);
-}
+};
 
 Compliment.find = function(id){
   return compliments[parseInt(id) - 1]
-}
+};
 
 module.exports = Compliment;
